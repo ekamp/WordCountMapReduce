417d21e9ddf22249530b99dd4d9dbd7be60dcd2d
Hua Yang <huacyang@gmail.com>
2014-03-27 00:11:56 -0400

    starting on the hash table

--- mapred.c
+++ mapred.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/syscall.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <ctype.h>
-#include "uthash.h"
-
 /*
  * @author: Hua Yang
  * @RUID: 128-00-2637
@@ -16,98 +7,124 @@
  * @RUID: 140-00-9007
  */
 
-//Initializing the struct to be used for the hashmap
-/*struct wordDictionary
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "uthash.h"
+
+// Initializing the struct to be used for the hashmap
+struct
+wordDictionary
 {
-	char * word; //Using this as the key, this will be the word itself
-	int value;
+	char* key; // using the word as the key
+	int value; // number of occurrence of the word
 	UT_hash_handle hashHandle;	
-};typedef struct wordDictionary *wordDictionaryPtr;
-*/
-/*
-	@param word : the desired word that one wishes to know the value of it is the ket and this will get the value
-*/
+}; typedef struct wordDictionary *wordDictionaryPtr;
 
-/*wordDictionaryPtr *dictionaryPointer = NULL;
-
-	wordDictionaryPtr findWord(char * word)
-	{
-		wordDictionaryPtr *myDiction;
-		HASH_FIND_STR(dictionaryPointer,&word,myDiction);
-		return myDiction;
-	}
-*/
 /*
- *	@param addedWord : word to be added to the hash table
+ * @param word : the desired word that one wishes to know the value of it is the ket and this will get the value
  */
+wordDictionaryPtr dictionaryPointer = NULL;
 
-/*	void addWordOccurance(wordDictionaryPtr *addedWord)
-	{
-		HASH_ADD_STR(dictionaryPointer,addedWord->word,addedWord);
-	}
-*/
-/*
-	@param fileName : the name of the file to be split into new files containing each 1000 orso to be read faster by the mappers
-	@param numberOfPieces : the number of pieces the file will be split into. This will coorespond to the number of mappers so that one mapper gets one piece
-*/
-
-void splitFile(char * fileName, char * numberOfPieces)
+wordDictionaryPtr findWord(char* word)
 {
-    if(fork() == 0)
-    {
-    	//Need to null terminate the array of inputs, ugh -________-
-    	char *splitFile[]={"./split.sh",fileName,numberOfPieces,NULL};
-    	//printf("The file name is %s and the number of pieces %s\n",fileName,numberOfPieces);
-	    if (execvp("./split.sh",splitFile) < 0)
-	    {
+	wordDictionaryPtr ptr;
+	HASH_FIND_STR(dictionaryPointer, &word, ptr);
+	return ptr;
+}
+
+/*
+ * Create a new hash struct with the given word,
+ *   then insert into the main hash.
+ * @param word : word to be added to the hash table
+ */
+void
+addWord(char* word)
+{
+	wordDictionaryPtr addWord;
+	addWord = malloc(sizeof(struct wordDictionary));
+	addWord->key = (char*) calloc(strlen(word), sizeof(char));
+	strcpy(addWord->key, word);
+	addWord->value = 1;
+
+	HASH_ADD_STR(dictionaryPointer, key, addedWord);
+}
+
+void
+
+/*
+ * @param fileName : the name of the file to be split into new files containing each 1000 orso to be read faster by the mappers
+ * @param numberOfPieces : the number of pieces the file will be split into. This will coorespond to the number of mappers so that one mapper gets one piece
+ */
+void
+deleteGeneratedFiles(char* file)
+{
+	int len = strlen(file)+5;
+	char* cmd = (char*) calloc(len, sizeof(char));
+	strcat(cmd, "rm ");
+	strcat(cmd, file);
+	strcat(cmd, ".*");
+	cmd[len] = '\0';
+	system(cmd);
+}
+
+void
+splitFile(char* fileName, char* numberOfPieces)
+{
+	pid_t childLabor;
+	childLabor = fork();
+    if (childLabor == 0) {
+    	char* splitFile[] = {"./split.sh", fileName, numberOfPieces, '\0'};
+
+	    if (execvp("./split.sh",splitFile) < 0) {
 	    	perror("./split.sh");
 	    }
-    }
-    else
-    {
-    	//Parent thread
+    } else if (childLabor < 0) {
+    	perror("child");
+    } else {
+    	int returnStatus;    
+   		waitpid(childLabor, &returnStatus, 0);
     }
 }
 
 
 /*
-@param name : String that contains the name of the input file
-
-This funciton reads in a file and prints it to the console taking in the name of the file as input
-*/
-
-
-void readFile(char* name){
+ * This funciton reads in a file and prints it to the console taking in the name of the file as input.
+ * @param name : String that contains the name of the input file
+ */
+void
+readFile(char* name) 
+{
 	int c;
 	FILE * fileName;
 	struct dirent *data;
 	char * nextName;
 	char * relPath;
 
-	
-
 	//Check to make sure the name is legal
-	if(name==NULL||name[strlen(name)-1]=='.')
-	{
+	if(name==NULL||name[strlen(name)-1]=='.') {
 		return;
 	}
 	//Make sure the file exists
-	if ((fileName = fopen(name,"r")) != NULL)
-	{
+	if ((fileName = fopen(name,"r")) != NULL) {
 			//Loop over the characters of the file in order to print them to the console
 			while ((c = getc(fileName)) != EOF)
         		printf("%c",c);
         	//Close the file after reading
 			fclose(fileName);
 	}
-	else
-	{
+	else {
 		fprintf(stderr , "ERROR: %s is not a file or directory.\n", name);
 		return;
 	}
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char** argv)
 {
 	FILE *output;
 	char safety;
@@ -120,7 +137,7 @@ int main(int argc, char **argv)
 	char * numberOfMappers;
 	int numberOfReducers;
 
-	//Inputs from the command line are as follows (11 inputs including the .o file)
+	// inputs from the command line are as follows (11 inputs including the .o file)
 	//-a [wordcount,sort]   : states whether the user wants to use the wordcount mapred or int sort mapred (typeOfRun)
 	//-i [procs,threads]    : states whether the user wishes to use processes or threads (threadOrProc)
 	//-m num_maps 			: provides the number of mappers for the program (numberOfMappers)
@@ -128,7 +145,7 @@ int main(int argc, char **argv)
 	//infile 				: the file to be read in by the program (infile)
 	//outfile				: the file to be writen to by the program (outfile)
 
-	//Assign the variables from the input taken from the command line args
+	// assign the variables from the input taken from the command line args
 	typeOfRun = argv[2];
 	threadOrProc = argv[4];
 	numberOfMappers = argv[6];
@@ -136,39 +153,41 @@ int main(int argc, char **argv)
 	infile = argv[9];
 	outfile = argv[10];
 
-
 	printf("Type of run is : %s\nWe are running on : %s\nThe number of mappers we have is : %s\nThe number of reducers is : %i\nThe input file name is : %s\nThe output file name is : %s\nThe number of args we have is : %i\n",
 		typeOfRun,threadOrProc,numberOfMappers,numberOfReducers,infile,outfile,argc);
 
-	/*If incorrect number of arguments */
-	if (argc != 11)
-	{
+	// if incorrect number of arguments
+	if (argc != 11) {
 		fprintf(stderr, "ERROR: Incorrect number of arguments\n");
 		return 0;
 	}
 
-	/*disallow overwiting our source files in the output*/
-	if (strcmp(outfile,"mapred.o")==0||strcmp(outfile,"Makefile")==0||strcmp(outfile,"readme.pdf")==0)
-	{
+	// disallow overwiting our source files in the output
+	if (strcmp(outfile,"mapred.o") == 0 ||
+		strcmp(outfile,"Makefile") == 0 ||
+		strcmp(outfile,"readme.pdf") == 0) {
 		fprintf(stderr, "Please don't try to overwrite our sourcefiles with the output\n");
 		return 0;
 	}
 
-	if ((output=fopen(outfile,"r"))!=NULL){
+	if ((output = fopen(outfile,"r")) != NULL) {
 		fclose(output);
-		fprintf(stdout, "The file designated for output already exists, are you sure you wish to overwite it? (y/n)");
-		if(fscanf(stdin, "%c%c", &safety, &tooMany)!=2||safety!='y'||tooMany!='\n'){
+		fprintf(stdout, "The file designated for output already exists, are you sure you wish to overwite it? (y/n): ");
+		if(fscanf(stdin, "%c%c", &safety, &tooMany)!=2||safety!='y'||tooMany!='\n') {
 			fprintf(stdout, "Ending program.\n");
 			return 0;
 		}
 	}
 	
-	//Set the out file as the first argument from the commandline
+	// set the out file as the first argument from the commandline
 	output = fopen(outfile,"w");
-	// Before reading in the file make sure to split, the file in 25 or less parts, in his example he uses 25 soooo im just going to use it for now
-	splitFile(infile,numberOfMappers); 
-	//Print the file specified by the second argument in the command line uncomment this if you want to print the file
+	// before reading in the file make sure to split, the file in 25 or less parts, in his example he uses 25 soooo im just going to use it for now
+	splitFile(infile,numberOfMappers);
+
+	// print the file specified by the second argument in the command line uncomment this if you want to print the file
 	//readFile(infile);
 	fclose(output);
+
+	deleteGeneratedFiles(infile);
 	return 0;
 }
